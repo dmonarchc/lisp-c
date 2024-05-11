56abe159a27abe579bf30fe24348a3966927f644
dmonarchc <cisternasdelriodaniel@gmail.com>
2024-05-11 06:18:49 -0400

    Add doge_code and doge_grammar

--- src/testing-compiler/hello_world.c
+++ src/testing-compiler/hello_world.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void helloPrint(int nTimes){
+  int count = 0;
+  while(nTimes>count){
+    puts("Hello, world!");
+    count++;
+  }
+}
+
 int main(int argc, char** argv) {
-  puts("Hello, world!");
+
+//  for(int i=0; i<5; i++){
+//    puts("Hello, world!");
+//  }
+  helloPrint(5);
   return 0;
 }
